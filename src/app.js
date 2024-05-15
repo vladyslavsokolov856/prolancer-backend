ea7aad8660fb52d7c3aeee7d92c61d5dd819b376
Vladyslav Sokolov <sokolovvladyslav856@gmail.com>
2024-05-15 15:35:31 -0500

    .env.example && specify PORT in .env

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const sequelize = require("./utils/db");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
