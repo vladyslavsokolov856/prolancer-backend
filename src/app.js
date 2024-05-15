c5c196be164edde360ac20b68dfddec865113d3e
Vladyslav Sokolov <sokolovvladyslav856@gmail.com>
2024-05-15 16:17:20 -0500

    fix heroku port assignment

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const sequelize = require("./utils/db");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = process.argv.includes('--port') ? parseInt(process.argv[process.argv.indexOf('--port') + 1]) : 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
