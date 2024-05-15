a6d45bb4aad690fe2db75f9c29a5fbf20f56e37f
Vladyslav Sokolov <sokolovvladyslav856@gmail.com>
2024-05-15 15:34:26 -0500

    customer.js reformat code style

--- src/models/Customer.js
+++ src/models/Customer.js
@@ -1,76 +1,76 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../utils/db');
+const { DataTypes } = require("sequelize");
+const sequelize = require("../utils/db");
 
-const Customer = sequelize.define('customer', {
+const Customer = sequelize.define("customer", {
   id: {
     type: DataTypes.UUID,
     defaultValue: DataTypes.UUIDV4,
-    primaryKey: true
+    primaryKey: true,
   },
   user_id: {
     type: DataTypes.UUID,
     allowNull: false,
     references: {
-      model: 'users',
-      key: 'id'
-    }
+      model: "users",
+      key: "id",
+    },
   },
   type: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
   },
   language: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
   },
   company_name: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
   },
   name_contact_person: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
   },
   email_contact_person: {
     type: DataTypes.STRING,
     allowNull: false,
-    unique: true
+    unique: true,
   },
   phone_contact_person: {
     type: DataTypes.STRING,
     allowNull: false,
-    unique: true
+    unique: true,
   },
   address: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
   },
   city: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
   },
   postal_code: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
   },
   country: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
   },
   payment_due_days: {
     type: DataTypes.INTEGER,
-    allowNull: false
+    allowNull: false,
   },
   company_id: {
     type: DataTypes.STRING,
     allowNull: false,
-    unique: true
+    unique: true,
   },
   ean: {
     type: DataTypes.STRING,
     allowNull: false,
-    unique: true
-  }
+    unique: true,
+  },
 });
 
 module.exports = Customer;
