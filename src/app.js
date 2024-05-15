f0ae4ea2a6e4d2ab51394885aeb6e334b99a776a
Vladyslav Sokolov <sokolovvladyslav856@gmail.com>
2024-05-15 16:22:11 -0500

    add '/'endpoint for test

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const sequelize = require("./utils/db");
 const bodyParser = require("body-parser");
+const User = require("./models/User");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+  try {
+    const users = await User.findAll({
+      attributes: ["id", "email", "phone", "first_name", "last_name"],
+    });
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
 app.listen(port, async () => {
   console.log(`Server is running on port ${port}`);
