c80a1ab79041f56a77ac04c5730a83017ce40b20
Vladyslav Sokolov <sokolovvladyslav856@gmail.com>
2024-05-21 20:57:40 +0800

    implement invoices crud

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const apiRouter = express.Router();
 const customerRouter = require("./customer");
 const userRouter = require("./user");
+const invoiceRouter = require("./invoice");
 
 apiRouter.use("/customers", customerRouter);
 apiRouter.use("/users", userRouter);
+apiRouter.use("/invoices", invoiceRouter);
 
 module.exports = apiRouter;
