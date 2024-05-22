5349e43f0f2c62e8122f737f731fecc8c92a3709
Vladyslav Sokolov <sokolovvladyslav856@gmail.com>
2024-05-22 23:48:35 +0800

    implement work logs crud

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,11 @@ const apiRouter = express.Router();
 const customerRouter = require("./customer");
 const userRouter = require("./user");
 const invoiceRouter = require("./invoice");
+const workLogRouter = require("./workLog");
 
 apiRouter.use("/customers", customerRouter);
 apiRouter.use("/users", userRouter);
 apiRouter.use("/invoices", invoiceRouter);
+apiRouter.use("/work-logs", workLogRouter);
 
 module.exports = apiRouter;
