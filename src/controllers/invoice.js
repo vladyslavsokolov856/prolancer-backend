5f582d1a9832c80043369227358699d6c43eadb4
Vladyslav Sokolov <sokolovvladyslav856@gmail.com>
2024-05-28 21:52:32 -0500

    update invoice controller

--- src/controllers/invoice.js
+++ src/controllers/invoice.js
@@ -1,11 +1,5 @@
 const pgp = require("pg-promise")();
-const {
-  getAll,
-  getById,
-  create,
-  updateById,
-  deleteById,
-} = require("./genericControler");
+const { getAll, getById, deleteById } = require("./genericControler");
 const pool = require("../utils/db");
 
 const BASE_TABLE = "invoices";
@@ -42,11 +36,100 @@ const invoicesController = {
 
       res.status(201).json(result[0]);
     } catch (err) {
+      console.log(err);
       res.status(400).json({ error: err });
     }
   },
-  updateById: (req, res) => updateById(req, res, BASE_TABLE),
-  deleteById: (req, res) => deleteById(req, res, BASE_TABLE),
+  updateById: async (req, res) => {
+    const invoiceId = req.params.id;
+
+    const { order_lines, id, deleted, ...rest } = req.body;
+
+    const updates = Object.entries(rest)
+      .map(([key, value]) => `${key} = $${Object.keys(rest).indexOf(key) + 1}`)
+      .join(", ");
+
+    try {
+      const result =
+        (await pool.query(
+          `UPDATE "${BASE_TABLE}" SET ${updates} WHERE id = $${
+            Object.keys(rest).length + 1
+          } AND deleted IS false RETURNING *`,
+          [...Object.values(rest), id]
+        )) + `WHERE id = ${invoiceId} AND status = draft`;
+
+      const insertColumns = new pgp.helpers.ColumnSet(
+        ["description", "quantity", "unit_price", "invoice_id"],
+        { table: "order_lines" }
+      );
+
+      const newLines = order_lines
+        .filter(({ id }) => !id)
+        .map((order) => ({
+          ...order,
+          invoice_id: invoiceId,
+        }));
+      const insertLines =
+        newLines.length && pgp.helpers.insert(newLines, insertColumns);
+
+      const updateColumns = new pgp.helpers.ColumnSet(
+        ["description", "quantity", "unit_price", "invoice_id", "id"],
+        { table: "order_lines" }
+      );
+      const oldLines = order_lines.filter(({ id }) => id);
+      const updateLines =
+        oldLines.length &&
+        pgp.helpers.update(oldLines, updateColumns) + ` WHERE v."id" = t.id`;
+
+      await pool.tx(async (t) => {
+        const idsInArray = order_lines.map((d) => d.id);
+        if (idsInArray.length) {
+          await t.none(
+            "DELETE FROM order_lines WHERE id NOT IN (${ids:csv})" +
+              ` AND invoice_id = ${invoiceId}`,
+            {
+              ids: idsInArray,
+            }
+          );
+        } else {
+          await t.none(
+            "DELETE FROM order_lines WHERE" + ` invoice_id = ${invoiceId}`
+          );
+        }
+
+        if (insertLines) await t.none(insertLines);
+        if (updateLines) await t.none(updateLines);
+      });
+
+      res.status(201).json(result[0]);
+    } catch (err) {
+      console.log(err);
+      res.status(400).json({ error: err });
+    }
+  },
+  deleteById: async (req, res) => {
+    const id = parseInt(req.params.id);
+
+    try {
+      const result = await pool.query(
+        `SELECT * FROM ${BASE_TABLE} WHERE id = ${id} AND deleted IS false`
+      );
+
+      if (!(result.length > 0)) {
+        res.status(404).json({ error: "Record not found" });
+        return;
+      }
+
+      const invoice = result[0];
+      if (invoice.status === "draft") {
+        return deleteById(req, res, BASE_TABLE);
+      }
+
+      res.status(400).json({ error: "Only draft invoices can be deleted." });
+    } catch (err) {
+      res.status(400).json({ error: err });
+    }
+  },
 };
 
 module.exports = invoicesController;
