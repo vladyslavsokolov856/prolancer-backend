b14b4327c526bba4cefdd57f79f24e46e9ad693c
Vladyslav Sokolov <sokolovvladyslav856@gmail.com>
2024-09-11 21:52:16 +0200

    modify task validator

--- src/validators/tasks.js
+++ src/validators/tasks.js
@@ -1,115 +1,129 @@
 const { body } = require("express-validator");
 
 const taskValidators = [
-  body("allow_mileages")
+  body("request_allow_mileages")
     .optional({ nullable: true })
     .isBoolean()
     .withMessage('allow_mileages must be a boolean value.'),
-  body("archived")
-    .isBoolean()
-    .withMessage('archived must be a boolean value.'),
   body('currency')
-    .isString()
-    .withMessage('currency must be a string.')
+    .isLength({ min: 1 })
+    .withMessage('Currency must be required.')
+    .isLength({ max: 3 })
+    .withMessage('Currency must be a valid string.')
     .trim()
     .customSanitizer((value) => value.toUpperCase()),
   body('customer_id')
-    .isInt({ min: 1 })
-    .withMessage('customer_id must be a positive integer.'),
+    .isLength({ min: 1 })
+    .withMessage('Customer must be required.')
+    .isString()
+    .customSanitizer(value => {
+      return parseInt(value.replace(/\D/g, ''), 10);
+    })
+    .isInt()
+    .withMessage('customer_id must be a valid integer'),
   body('customer_message')
-    .optional()
+    .isLength({ min: 1 })
+    .withMessage('Customer message must be required.')
     .isString()
     .withMessage('customer_message must be a string.'),
   body('deleted')
     .isBoolean()
     .withMessage('deleted must be a boolean value.'),
   body('description')
-    .optional()
+    .isLength({ min: 1 })
+    .withMessage('Description must be required.')
     .isString()
     .withMessage('description must be a string.'),
   body('end_date')
+    .isLength({ min: 1 })
+    .withMessage('End date must be required.')
+    .isString()
+    .customSanitizer(value => {
+      const date = new Date(value);
+      return isNaN(date.getTime()) ? null : date.toISOString().split('T')[0];
+    })
     .isISO8601()
-    .withMessage('end_date must be a valid ISO 8601 date.'),
+    .withMessage('End date must be a valid date'),
   body('expected_minutes')
-    .isInt({ min: 0 })
-    .withMessage('expected_minutes must be a non-negative integer.'),
-    body('id')
-    .isInt({ min: 1 })
-    .withMessage('id must be a positive integer.'),
-  body('identifier')
-    .isUUID()
-    .customSanitizer((value) => value.replace(/\D/g, ""))
-    .withMessage('identifier must be a valid UUID.'),
-  body('is_retainer')
-    .isBoolean()
-    .optional({ nullable: true })
-    .withMessage('is_retainer must be a boolean value.'),
-  body('job_type_id')
-    .isInt({ min: 1 })
-    .withMessage('job_type_id must be a positive integer.'),
-  body('minutes_spent')
-    .optional({ nullable: true })
-    .isInt({ min: 0 })
-    .withMessage('minutes_spent must be a non-negative integer if provided.'),
-  body('offer_id')
-    .optional({ nullable: true })
+    .isLength({ min: 1 })
+    .withMessage('Expected minutes must be required.')
+    .isString()
+    .customSanitizer(value => {
+      return parseInt(value.replace(/\D/g, ''), 10);
+    })
     .isInt()
-    .customSanitizer((value) => value.replace(/\D/g, "") || null)
-    .withMessage('offer_id must be an integer.'),
+    .withMessage('expected_minutes must be a valid integer'),
+  body('is_retainer')
+    .optional({ nullable: true })
+    .isBoolean()
+    .withMessage('Retainer must be a boolean value.'),
+  body('job_type_id')
+    .isLength({ min: 1 })
+    .withMessage('Job type must be required.')
+    .isString()
+    .customSanitizer(value => {
+      return parseInt(value.replace(/\D/g, ''), 10);
+    })
+    .isInt()
+    .withMessage('job_type_id must be a valid integer'),
   body('payment_amount')
+    .isLength({ min: 1 })
+    .withMessage('Payment amount must be required.')
+    .isString()
+    .customSanitizer(value => {
+      const sanitizedValue = value.replace(/\D/g, '');
+      return sanitizedValue ? Number(sanitizedValue) : null;
+    })
     .isNumeric()
-    .withMessage('payment_amount must be a numeric value.')
-    .customSanitizer((value) => value.replace(/[^\d.]/g, "")),
+    .withMessage('payment_amount must be a numeric value.'),
   body('payment_term')
     .isString()
     .isIn(["current_month", "ongoing_week", "other", "task_end"])
-    .withMessage('payment_term must be a string.'),
+    .withMessage('payment_term must be either "Current month" or "Ongoing week", "Other", "Task end".'),
   body('payment_term_days')
-    .isInt({ min: 0 })
-    .withMessage('payment_term_days must be a non-negative integer.'),
-  body('payment_term_other')
-    .optional({ nullable: true })
+    .isLength({ min: 1 })
+    .withMessage('Payment term days must be required.')
     .isString()
-    .withMessage('payment_term_other must be a string.'),
+    .customSanitizer(value => {
+      return parseInt(value.replace(/\D/g, ''), 10);
+    })
+    .isInt()
+    .withMessage('payment_term_days must be a valid integer'),
   body('payment_type')
     .isString()
     .isIn(["per_day", "per_hour", "project_price"])
-    .withMessage('payment_type must be a string.'),
+    .withMessage('payment_type must be either "Per day" or "Per hour", "Project price".'),
   body('purchase_order_number')
-    .optional()
+    .optional({ nullable: true })
     .isString()
-    .withMessage('purchase_order_number must be a string.'),
+    .withMessage('PO number must be a string.'),
   body('reference')
-    .optional()
+    .optional({ nullable: true })
     .isString()
     .withMessage('reference must be a string.'),
-  body('request_allow_mileages')
-    .isBoolean()
-    .withMessage('request_allow_mileages must be a boolean value.'),
-  body('requested_changes')
-    .optional({ nullable: true })
-    .isString()
-    .withMessage('requested_changes must be a string.'),
   body('show_customer_price')
-    .isBoolean()
     .optional({ nullable: true })
+    .isBoolean()
     .withMessage('show_customer_price must be a boolean value.'),
   body('start_date')
+    .isLength({ min: 1 })
+    .withMessage('Start date must be required.')
+    .isString()
+    .customSanitizer(value => {
+      const date = new Date(value);
+      return isNaN(date.getTime()) ? null : date.toISOString().split('T')[0];
+    })
     .isISO8601()
-    .withMessage('start_date must be a valid ISO 8601 date.'),
+    .withMessage('Start date must be a valid date'),
   body('status')
-    .isString()
-    .withMessage('status must be a string.'),
-  body('terms_accepted')
-    .isBoolean()
-    .withMessage('terms_accepted must be a boolean value.'),
+    .isIn(["sent", "draft"])
+    .withMessage('status must be either "sent" or "draft".'),
   body('title')
+    .isLength({ min: 1 })
+    .withMessage("title is required.")
     .isString()
-    .trim() 
-    .withMessage('title must be a string.'),
-  body('user_id')
-    .isInt({ min: 1 })
-    .withMessage('user_id must be a positive integer.'),
+    .withMessage('title must be a string.')
+    .trim(),
 ];
 
 module.exports = taskValidators;
